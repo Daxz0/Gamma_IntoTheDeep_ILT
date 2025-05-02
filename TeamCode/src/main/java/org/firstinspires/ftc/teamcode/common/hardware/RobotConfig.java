994371cbcfa5f833b8ead1e93f2150b05deaeee8
Daxz <59543649+Daxz0@users.noreply.github.com>
2025-05-02 16:39:08 -0700

    5/2/25 Update
    Moved config to RobotConfig
    Added Linear Rail Control

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware/RobotConfig.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.common.hardware;
+
+public class RobotConfig {
+
+    public static int ARM_POSITION_INIT = 300;
+    public static int ARM_POSITION_INTAKE = 450;
+    public static int ARM_POSITION_BASKET = 2500;
+    public static int ARM_POSITION_RELEASE = 2500;
+    public static double LINEAR_SLIDE_UP_POWER = -0.8;
+    public static double LINEAR_SLIDE_DOWN_POWER = 0.8;
+    public static double RAIL_POSITION_DOWN = 0;
+    public static double RAIL_POWER = 0.8;
+    public static double RAIL_POSITION_UP = 0.8;
+    public static double CLAW_CLOSE = 0.5;
+    public static double CLAW_OPEN = 0;
+
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/OpMode/OpMode3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/OpMode/OpMode3.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.common.hardware.RobotConfig;
 import org.firstinspires.ftc.teamcode.common.hardware.RobotHardware;
 
 @TeleOp(name = "OpMode3")
@@ -12,33 +13,28 @@ public class OpMode3 extends LinearOpMode {
 
     RobotHardware robot = new RobotHardware();
 
-    private DcMotor arm = null;
-    private DcMotor rail = null;
-    private Servo claw = null;
-
-    private static final int ARM_POSITION_INIT = 300;
-    private static final int ARM_POSITION_INTAKE = 450;
-    private static final int ARM_POSITION_BASKET = 2500;
-
-    private static final double CLAW_CLOSE = 0.5;
-    private static final double CLAW_OPEN = 0;
-
     private enum RobotState {
         INIT,
         INTAKE,
-        BASKET
+        BASKET,
+        RELEASE
     }
 
     private RobotState currentState = RobotState.INIT;
     private boolean clawState = false;
     private boolean previousA = false;
+    private boolean railUp = false;
+    private boolean previousB = false;
 
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap);
-        arm = robot.motorarm;
-        claw = robot.servoclaw;
-        rail = robot.motorlinearRail;
+        DcMotor arm = robot.motorarm;
+        Servo claw = robot.servoclaw;
+        DcMotor rail = robot.motorlinearRail;
+
+        rail.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rail.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -50,28 +46,66 @@ public class OpMode3 extends LinearOpMode {
             // --- CONTROLS ---
             // A = toggle claw
             // B = toggle linear rail
-            // X = claw to INIT position
-            // Y = claw to INTAKE position
+            // DOWN-DPAD = lower linear rail
+            // UP-DPAD = raise linear rail
+
+            // --- STATE CONTROLS ---
+            // X = INIT state
+            // Y = INTAKE state
+            // L-BUMPER = BASKET state
+            // R-BUMPER = RELEASE state
 
             if (gamepad1.x) {
                 currentState = RobotState.INIT;
             } else if (gamepad1.y) {
                 currentState = RobotState.INTAKE;
+            } else if (gamepad1.left_bumper) {
+                currentState = RobotState.BASKET;
+            } else if (gamepad1.right_bumper) {
+                currentState = RobotState.RELEASE;
             }
 
+            // --- CLAW TOGGLE ---
             if (gamepad1.a && !previousA) {
                 clawState = !clawState;
-                claw.setPosition(clawState ? CLAW_CLOSE : CLAW_OPEN);
+                claw.setPosition(clawState ? RobotConfig.CLAW_CLOSE : RobotConfig.CLAW_OPEN);
             }
             previousA = gamepad1.a;
 
+            // --- RAIL TOGGLE ---
+            if (gamepad1.b && !previousB) {
+                railUp = !railUp;
+
+                rail.setTargetPosition((int) (railUp ? RobotConfig.RAIL_POSITION_UP : RobotConfig.RAIL_POSITION_DOWN));
+                rail.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                rail.setPower(RobotConfig.RAIL_POWER);
+            }
+            previousB = gamepad1.b;
+
+            if (gamepad1.dpad_down) {
+                rail.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                rail.setPower(RobotConfig.LINEAR_SLIDE_DOWN_POWER);
+            } else if (gamepad1.dpad_up) {
+                rail.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                rail.setPower(RobotConfig.LINEAR_SLIDE_UP_POWER);
+            }
+
+            // --- ARM POSITION SETTING ---
             int armTarget = 0;
             switch (currentState) {
                 case INIT:
-                    armTarget = ARM_POSITION_INIT;
+                    armTarget = RobotConfig.ARM_POSITION_INIT;
+                    rail.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    rail.setPower(RobotConfig.LINEAR_SLIDE_DOWN_POWER);
                     break;
                 case INTAKE:
-                    armTarget = ARM_POSITION_INTAKE;
+                    armTarget = RobotConfig.ARM_POSITION_INTAKE;
+                    break;
+                case BASKET:
+                    armTarget = RobotConfig.ARM_POSITION_BASKET;
+                    break;
+                case RELEASE:
+                    armTarget = RobotConfig.ARM_POSITION_RELEASE;
                     break;
             }
 
@@ -79,6 +113,7 @@ public class OpMode3 extends LinearOpMode {
             arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             arm.setPower(0.5);
 
+            // --- DRIVE CONTROL ---
             double horizontal = -gamepad1.right_stick_x * 0.6;
             double vertical = gamepad1.right_stick_y * 0.6;
             double turn = -gamepad1.left_stick_x * 0.6;
@@ -88,16 +123,16 @@ public class OpMode3 extends LinearOpMode {
             double blPower = vertical + turn - horizontal;
             double brPower = vertical - turn + horizontal;
 
-            double max = Math.max(1.0,
-                    Math.max(Math.abs(flPower),
-                            Math.max(Math.abs(frPower),
-                                    Math.max(Math.abs(blPower), Math.abs(brPower)))));
+            double max = Math.max(1.0, Math.max(Math.abs(flPower),
+                    Math.max(Math.abs(frPower), Math.max(Math.abs(blPower), Math.abs(brPower)))));
 
             robot.setDrivePower(flPower / max, frPower / max, blPower / max, brPower / max);
 
+            // --- TELEMETRY ---
             telemetry.addData("Claw", clawState ? "Closed" : "Open");
             telemetry.addData("Arm Target", armTarget);
             telemetry.addData("Arm Position", arm.getCurrentPosition());
+            telemetry.addData("Rail Direction", railUp ? "Up" : "Down");
             telemetry.update();
         }
     }
